f169b1f202e53663542fd87cb849e34123522951
Luke Perron <LukeDPerron@users.noreply.github.com>
2025-10-28 03:11:40 +0000

    feat: styled text to be different colors in story and differentiated important text

--- src/adventure/story.py
+++ src/adventure/story.py
@@ -1,5 +1,8 @@
 from adventure.utils import read_events_from_file
 import random
+from rich import print
+from rich.style import Style
+from rich.console import Console
 
 def step(choice: str, events):
     random_event = random.choice(events)
@@ -12,17 +15,18 @@ def step(choice: str, events):
         return "You stand still, unsure what to do. The forest swallows you."
 
 def left_path(event):
-    return "You walk left. " + event
+    return "You walk left. " + "[red]" + event
 
 def right_path(event):
-    return "You walk right. " + event
+    return "You walk right. " + "[purple]" + event
 
 if __name__ == "__main__":
     events = read_events_from_file('events.txt')
 
     print("You wake up in a dark forest. You can go left or right.")
     while True:
-        choice = input("Which direction do you choose? (left/right/exit): ")
+        console = Console()
+        choice = console.input("Which direction do you choose?[italic bold blue][warning](left/right/exit): [/ italic bold blue]")
         choice = choice.strip().lower()
         if choice == 'exit':
             break
